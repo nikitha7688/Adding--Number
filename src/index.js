b9cb1904267f04cd73ed7daeedad056c121ca3cc
nikitha7688 <nikitha7688@gmail.com>
2024-09-20 07:27:28 +0530

    update project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./style.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root =ReactDOM.createRoot(document.getElementById("root"))
+
+function Counter()
+{
+    const [firstNumber,setfirstNumber] = useState("");
+    const [secondNumber,setscoundNumber] = useState("");
+    const [result,setResult] = useState("")
+
+     const handlefirstNumber=(evt) =>{
+        setfirstNumber(evt.target.value)
+     }
+
+     const handlescoundNumber=(evt) =>{
+        setscoundNumber(evt.target.value)
+     }
+
+     const handleResult =(evt) =>{
+        evt.preventDefault(); 
+        const sum = Number(firstNumber) + Number(secondNumber); 
+        setResult(sum); 
+     }
+
+
+     return(
+    <div class="card">
+        <h1>Add two Numbers</h1>
+
+        <form>
+         <h2>First Number:</h2>
+        <input class="inputbox" value={firstNumber} onChange={handlefirstNumber} type="number" placeholder="Enter First Number"></input><br/>
+
+        <h2>Second Number:</h2>
+        <input class="inputbox" value={secondNumber} onChange={handlescoundNumber} type="number" placeholder="Enter Second Number"></input><br/>
+
+        <button  class="button" onClick={handleResult}>Submit</button>
+    
+
+        </form>
+         <h1>Result:  {result}</h1>
+    </div>
+)
+}
+
+root.render(<Counter/>)
